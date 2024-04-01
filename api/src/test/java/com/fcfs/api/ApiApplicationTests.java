b5c8fd7ea28055a1f1adbe8c4baadf3509f8e016
ybchar <uiurihappy@sju.ac.kr>
2024-04-01 22:28:12 +0900

    init

---
+++ api/src/test/java/com/fcfs/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcfs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
